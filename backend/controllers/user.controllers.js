6934e4ebd42b16187cd0726074a16cce2fc56f45
YassineEF <yassineonline@outlook.fr>
2022-03-28 16:44:45 +0200

    :art:check login

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -7,15 +7,24 @@ module.exports.InscriptionUser = async function  (req, res) {
     let Email = req.body.Email
     let Password = req.body.Password
     let pwd =   await bcrypt.hash(Password,  await  bcrypt.genSaltSync(8))
-   db.query(
-    "INSERT INTO `admin` (`username`, `email`, `password`, `status`) VALUES (?, ?, ?, 1)",
-    [Username, Email, pwd],
-    function (err, rows) {
-      if (err) {
-        res.json({ msg: err });
-      } else {
-        res.json({ msg: "success", user: rows, });
-      }
-    }
-  );
+   
+    db.query("SELECT username, email FROM admin WHERE username = (?) OR email = (?) ", [Username, Email] ,function(err,result){
+        if(result.length === 0){
+            db.query(
+                "INSERT INTO `admin` (`username`, `email`, `password`, `status`) VALUES (?, ?, ?, 1)",
+                [Username, Email, pwd],
+                function (err, rows) {
+                  if (err) {
+                    res.json({ msg: err });
+                  } else {
+                    res.json({ msg: "success", user: rows, });
+                  }
+                }
+              );
+        }else if(err){
+            res.json({ msg: err })
+        }else{
+            res.json({ msg: err})
+        }
+    })
 };
